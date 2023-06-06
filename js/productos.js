f8d9cc4ab284f648d3d5de81b0c7135cac199055
juan morales <juanskate326@gmail.com>
2023-06-06 08:48:59 -0500

    init

---
+++ js/productos.js
@@ -0,0 +1,242 @@
+const muebles = [
+	"cocinasintegrales/muebles/IMG-20230602-WA0013.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0014.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0033.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0059.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0060.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0062.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0064.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0065.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0067.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0068.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0070.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0071.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0072.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0074.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0077.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0078.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0079.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0080.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0081.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0082.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0083.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0085.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0087.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0092.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0093.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0095.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0097.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0098.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0100.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0101.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0102.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0103.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0106.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0107.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0108.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0110.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0112.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0113.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0116.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0124.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0125.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0126.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0138.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0139.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0140.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0142.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0143.jpg",
+	"cocinasintegrales/muebles/IMG-20230602-WA0144.jpg",
+];
+const cocinas = [
+	"cocinasintegrales/cocinas/Imagen de WhatsApp 2023-06-01 a las 21.35.11.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0005.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0008.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0009.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0010.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0011.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0012.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0015.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0016.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0017.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0018.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0019.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0020.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0021.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0022.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0030.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0031.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0032.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0035.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0036.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0037.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0038.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0039.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0040.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0041.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0042.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0043.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0044.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0045.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0046.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0047.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0048.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0049.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0052.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0053.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0054.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0055.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0056.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0058.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0061.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0063.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0066.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0069.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0073.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0084.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0086.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0088.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0089.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0090.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0091.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0099.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0105.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0109.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0111.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0114.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0115.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0117.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0118.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0119.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0120.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0121.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0122.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0123.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0124.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0129.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0130.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0131.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0132.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0133.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0134.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0135.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0136.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0137.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0145.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0146.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0147.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0148.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0149.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0150.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0151.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0152.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0153.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0154.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0155.jpg",
+	"cocinasintegrales/cocinas/IMG-20230602-WA0156.jpg",
+];
+const banos = [
+	"cocinasintegrales/baños/0w7jcK-h2dIj84GvnSnYnzpcQN4MN6pggjvaqvISZoU=_plaintext_638213088332858965.jpg",
+	"cocinasintegrales/baños/IMG-20230602-WA0023.jpg",
+	"cocinasintegrales/baños/IMG-20230602-WA0024.jpg",
+	"cocinasintegrales/baños/IMG-20230602-WA0025.jpg",
+	"cocinasintegrales/baños/IMG-20230602-WA0026.jpg",
+	"cocinasintegrales/baños/IMG-20230602-WA0027.jpg",
+	"cocinasintegrales/baños/IMG-20230602-WA0028.jpg",
+	"cocinasintegrales/baños/IMG-20230602-WA0029.jpg",
+	"cocinasintegrales/baños/IMG-20230602-WA0034.jpg",
+	"cocinasintegrales/baños/IMG-20230602-WA0050.jpg",
+	"cocinasintegrales/baños/IMG-20230602-WA0051.jpg",
+	"cocinasintegrales/baños/IMG-20230602-WA0057.jpg",
+	"cocinasintegrales/baños/IMG-20230602-WA0075.jpg",
+	"cocinasintegrales/baños/IMG-20230602-WA0076.jpg",
+	"cocinasintegrales/baños/IMG-20230602-WA0094.jpg",
+	"cocinasintegrales/baños/IMG-20230602-WA0096.jpg",
+	"cocinasintegrales/baños/IMG-20230602-WA0104.jpg",
+	"cocinasintegrales/baños/IMG-20230602-WA0127.jpg",
+	"cocinasintegrales/baños/IMG-20230602-WA0128.jpg",
+	"cocinasintegrales/baños/IMG-20230602-WA0141.jpg",
+];
+
+const todos = [...cocinas, ...banos, ...muebles].sort();
+const collage = document.getElementById("collage");
+
+function createCard(element) {
+	const cardElement = document.createElement("div");
+
+	cardElement.setAttribute("class", "col-lg-4 col-md-12 col-sm-12 imagen");
+
+	cardElement.innerHTML = `
+              <div class="product-card position-relative pe-3 pb-3" role="button" onclick="openModal('{element}')" data-bs-target="#modaltoggle" data-bs-toggle="modal">
+                <div class="image-holder zoom-effect">
+                  <img src="{element}"
+                    alt="product-item" class="img-fluid rounded">
+                </div>
+
+                <div class="cart-detail d-flex justify-content-between align-items-center">
+                  <h3 class="card-title fs-6 pt-3">
+                    <a href="#">Running shoes for men</a>
+                  </h3>
+                  <span class="card-price fw-bold">$99</span>
+                </div>
+              </div>
+            `.replace(/{element}/g, element);
+
+	collage.appendChild(cardElement);
+}
+
+function filterByCocinas() {
+	clearAll();
+	for (let index = 0; index < cocinas.length; index++) {
+		const element = cocinas[index];
+		createCard(element);
+	}
+	reload();
+}
+function filterByBanos() {
+	clearAll();
+	for (let index = 0; index < banos.length; index++) {
+		const element = banos[index];
+		createCard(element);
+	}
+	reload();
+}
+function filterByMuebles() {
+	clearAll();
+	for (let index = 0; index < muebles.length; index++) {
+		const element = muebles[index];
+		createCard(element);
+	}
+	reload();
+}
+function filterByTodos() {
+	clearAll();
+	for (let index = 0; index < todos.length; index++) {
+		const element = todos[index];
+		createCard(element);
+	}
+	reload();
+}
+function clearAll() {
+	const children = collage.children;
+	while (children.length) {
+		children[0].parentNode.removeChild(children[0]);
+	}
+}
+
+function reload() {
+	var grid = document.querySelector("#collage");
+	var msnry = new Masonry(grid, {
+		itemSelector: ".imagen",
+	});
+}
+function openModal(id) {
+	console.log(id);
+	document.getElementById("imgModal").setAttribute("src", id);
+}
+window.onload = () => {
+	filterByTodos();
+	setTimeout(() => {
+		reload();
+	}, 1000);
+};
